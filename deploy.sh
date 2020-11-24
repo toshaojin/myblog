f403e07e0736ae3aa546a4830adaa971724b0055
金 邵 <jinshao@jindeMacBook-Pro.local>
2020-11-24 11:46:50 +0800

    rebuilding site 2020年11月24日 星期二 11时46分50秒 CST

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,6 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
 
 # Add changes to git.
 git add .
